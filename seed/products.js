aa1136d897abd52f39a9f2ca7d74be80502931f5
Steven Strumolo <stevenstrumolo@Stevens-MacBook-Air.local>
2021-02-09 15:14:36 -0500

    created routes models and helpers folder

---
+++ seed/products.js
@@ -0,0 +1,26 @@
+const db = require('../db/connection')
+const Product = require('../models/product')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const motorcycles = 
+    [
+        {
+          "name": "Product 001",
+          "imgURL": "https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          "price": "130",
+          "brand": ""
+        },
+    ]
+
+    await Product.insertMany(motorcycles)
+    console.log("Created motorcycles!")
+}
+const run = async () => {
+    await main()
+    db.close()
+}
+
+run()
