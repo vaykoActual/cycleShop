6f5e15f492655387850811f1121f3f3a649268e1
Varit Seekhao <varit.seekhao@gmail.com>
2021-02-16 10:26:25 -0500

    basic css

--- client/src/components/Brands/Brands.jsx
+++ client/src/components/Brands/Brands.jsx
@@ -3,53 +3,57 @@ import { NavLink } from "react-router-dom";
 
 const Brands = (props) => {
   return (
-    <div className="brands-image">
-      <div>
+    <div>
+      <div className="brands-container-arch">
         <img
-          className="main-image-img"
-          src="https://www.highreshdwallpapers.com/wp-content/uploads/2014/04/Yamaha-Motorcycle-Desktop-Wallpaper.jpg"
+          className="brands-image-arch"
+          src="https://www.archmotorcycle.com/assets/images/method-hero-1-min.jpg"
           alt="someText"
         />
-        <h1>Arch Method 143</h1>
-        <NavLink to="/brand/Arch"> shop </NavLink>
-        <NavLink to="#"> sell</NavLink>
+        <h1 className="brands-name-arch">Arch Method 143</h1>
+        <NavLink className="brands-shop" to="/brand/Arch">SHOP</NavLink>
+        <NavLink className="brands-sell" to="#">SELL</NavLink>
       </div>
 
-      <div>
-        <h1>2021 Road Glide</h1>
+      <div className="brands-container-harley">
+        <h1 className="brands-name">2021 Road Glide</h1>
         <img
+          className="brands-image-harley"
           src="https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2021/2021-road-glide/2021-road-glide-f16/360/2021-road-glide-f16-motorcycle-01.jpg?impolicy=myresize&rw=1600"
           alt="2021 Road Glide"
         />
-        <NavLink to="/brand/Harley Davidson"> shop </NavLink>
-        <NavLink to="#"> sell</NavLink>
+        <NavLink className="brands-shop" to="/brand/Harley Davidson">SHOP</NavLink>
+        <NavLink className="brands-sell" to="#">SELL</NavLink>
       </div>
-      <div>
-        <h1>YZF R1M</h1>
+      <div className="brands-container-yamaha">
+        <h1 className="brands-name">YZF R1M</h1>
         <img
+          className="brands-image-yamaha"
           src="https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5f4be31b2a0ab63a745a4beb&w=1920"
           alt="YZF R1M"
         />
-        <NavLink to="/brand/Yamaha"> shop </NavLink>
-        <NavLink to="#"> sell</NavLink>
+        <NavLink className="brands-shop" to="/brand/Yamaha">SHOP</NavLink>
+        <NavLink className="brands-sell" to="#">SELL</NavLink>
       </div>
-      <div>
-        <h1>2021 R nineT Scrambler</h1>
+      <div className="brands-container-bmw">
+        <h1 className="brands-name">2021 R nineT Scrambler</h1>
         <img
+          className="brands-image-bmw"
           src="https://2yrh403fk8vd1hz9ro2n46dd-wpengine.netdna-ssl.com/wp-content/uploads/2020/10/2021-BMW-R-nineT-lineup-First-look-Pure-GS-Scrambler-retro-motorcycles-9.jpg"
           alt="2021 R nineT Scrambler"
         />
-        <NavLink to="/brand/BMW"> shop </NavLink>
-        <NavLink to="#"> sell</NavLink>
+        <NavLink className="brands-shop" to="/brand/BMW">SHOP</NavLink>
+        <NavLink className="brands-sell" to="#">SELL</NavLink>
       </div>
-      <div>
-        <h1>DIAVEL 1260</h1>
+      <div className="brands-container-ducati">
+        <h1 className="brands-name">DIAVEL 1260</h1>
         <img
+          className="brands-image-ducati"
           src="https://images.ctfassets.net/x7j9qwvpvr5s/7pNrWMaSWtvv3H6aHqhEL6/09116590a18c241ed5c863ecc72add26/DVL-1260-Bl-MY21-Model-Preview-1050x650-v02.png"
           alt="DIAVEL 1260"
         />
-        <NavLink to="/brand/Ducati"> shop </NavLink>
-        <NavLink to="#"> sell</NavLink>
+        <NavLink className="brands-shop" to="/brand/Ducati">SHOP</NavLink>
+        <NavLink className="brands-sell"to="#">SELL</NavLink>
       </div>
     </div>
   );
--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,34 +1,39 @@
 import { NavLink } from "react-router-dom";
+import searchIcon from "../../../assets/icons/search-icon.svg";
+import logo from "../../../assets/icons/logo.svg";
+import hamburger from "../../../assets/icons/hamburger-menu.svg";
+
 import "./Nav.css";
 
 function Nav() {
   return (
     <div className="nav-container">
-      <div className="nav-logo">LOGO</div>
-      {/* <Search /> */}
-      <div className="nav-search">Search</div>
-      <div className="nav-navlinks-container">
-        <NavLink className="nav-navlinks-item" to="#">
-          Harley Davidson
+      <a className="nav-logo">
+        <img src={logo} alt="Logo" />
+      </a>
+        <NavLink className="nav-navlinks-harley" to="#">
+          HARLEY DAVIDSON
         </NavLink>
-        <NavLink className="nav-navlinks-item" to="#">
-          Ducati
+        <NavLink className="nav-navlinks-ducati" to="#">
+          DUCATI
         </NavLink>
-        <NavLink className="nav-navlinks-item" to="#">
-          Yamaha
+        <NavLink className="nav-navlinks-yamaha" to="#">
+          YAMAHA
         </NavLink>
-        <NavLink className="nav-navlinks-item" to="#">
-          Arch
+        <NavLink className="nav-navlinks-arch" to="#">
+          ARCH
         </NavLink>
-        <NavLink className="nav-navlinks-item" to="#">
+        <NavLink className="nav-navlinks-bmw" to="#">
           BMW
         </NavLink>
-        <NavLink className="nav-navlinks-item" to="#">
-          Account
+        <div className="nav-search">
+          <img src={searchIcon} alt="Search" />
+        </div>
+        <NavLink className="nav-navlinks-account" to="#">
+          ACCOUNT
         </NavLink>
-        <button>hamburger</button>
+        <div className="hamburger-menu"><img src={hamburger} alt="hamburger-menu"/></div>
       </div>
-    </div>
   );
 }
 
